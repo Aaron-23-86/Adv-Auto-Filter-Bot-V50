53586855d66f61d26087a96d8663cb6dbcf76247
Aaron-23-86 <88765008+Aaron-23-86@users.noreply.github.com>
2021-09-04 07:50:48 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,12 +5,17 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+<i>
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+You can call this as an Auto Filter Bot 
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+This is Version 2 of Auto Filter Bot
+
+Bot gives button link to files in connected channels on query !
+
+No need to add filters for your files or movies from now on!
+
+
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +48,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Termin_a_t_o_r">Aaron</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Channel</b> : <i><a href="https://t.me/hollywood_only">Click Me</a></i>
 """
